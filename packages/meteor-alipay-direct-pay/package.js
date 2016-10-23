8f205eaab1d6fb189ee0762a5f035db1f7bf10cc
pyj <pengyongjun@vip.qq.com>
2016-10-23 15:37:23 +0800

    add git

--- packages/meteor-alipay-direct-pay/package.js
+++ packages/meteor-alipay-direct-pay/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'alipay direct pay,alipay web api',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/yongjun925/meteor-alipay-direct-pay-example',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
